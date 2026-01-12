1c52597d0cd407f979eb254d6c918f0b6a3d2a20
BelattiMateo <mateobelatti7@gmail.com>
2026-01-12 12:13:14 -0300

    implementacion de controller getCursoById

--- SistemaDeCursosOnline/backend/src/routes/curso.routes.ts
+++ SistemaDeCursosOnline/backend/src/routes/curso.routes.ts
@@ -8,7 +8,7 @@ const cursoRouter = Router();
 cursoRouter.get('/cursos' ,CursosController.listCursos); // array de cursos 
 cursoRouter.get('/cursos/:idCurso',CursosController.getCursoById)
 cursoRouter.get('/cursos/profesor/:idProfesor', CursosController.getCursosByProfesor);
-cursoRouter.post('/cursos',authenticate, authorizeRole('ADMIN', '   PROFESOR'),  CursosController.crearCurso); // crear un curso
+cursoRouter.post('/cursos',authenticate, authorizeRole('ADMIN', 'PROFESOR'),  CursosController.crearCurso); // crear un curso
 cursoRouter.put('/cursos/:idCurso',authenticate, authorizeCursoEdit  ,CursosController.updateCurso); // edita un crso hay que pasarle el curso completo
 cursoRouter.delete('/cursos/:idCurso', authenticate, authorizeRole('ADMIN'), CursosController.deleteCurso); // elimina un curso
 
